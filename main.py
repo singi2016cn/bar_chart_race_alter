9fe132ebc3ffb62d891b6e15fd467028c7997ac1
yi.qin <yi.qin@xshoppy.com>
2020-12-29 11:56:52 +0800

    update main.py

--- main.py
+++ main.py
@@ -1,34 +1,24 @@
 import bar_chart_race as bcr
 import pandas as pd
+import os.path
 
-df = pd.read_csv("input/a.csv", index_col='date', parse_dates=['date'])
-bcr.bar_chart_race(
-    df=df,
-    filename='output/a.mp4',
-    orientation='h',
-    sort='desc',
-    n_bars=6,
-    fixed_order=False,
-    fixed_max=True,
-    steps_per_period=10,
-    interpolate_period=False,
-    label_bars=True,
-    bar_size=.95,
-    period_label={'x': .99, 'y': .25, 'ha': 'right', 'va': 'center'},
-    period_fmt='%B %d, %Y',
-    period_summary_func=lambda v, r: {'x': .99, 'y': .18,'s': f'Total deaths: {v.nlargest(6).sum():,.0f}','ha': 'right', 'size': 8, 'family': 'Courier New'},
-    perpendicular_bar_func='median',
-    period_length=500,
-    figsize=(5, 3),
-    dpi=144,
-    cmap='dark12',
-    title='COVID-19 Deaths by Country',
-    title_size='',
-    bar_label_size=7,
-    tick_label_size=7,
-    shared_fontdict={'family' : 'Helvetica', 'color' : '.1'},
-    scale='linear',
-    writer=None,
-    fig=None,
-    bar_kwargs={'alpha': .7},
-    filter_column_colors=False)  
+
+inputFile = 'input/a.csv'
+
+inputFileArr = os.path.splitext(os.path.basename(inputFile))
+inputFileName = inputFileArr[0]
+inputFileExt = inputFileArr[1]
+
+outputFile = 'output/' + inputFileName +'.mp4'
+index_col = 'date'
+parse_dates = [index_col]
+
+if inputFileExt == '.csv':
+    df = pd.read_csv(inputFile, index_col=index_col, parse_dates=parse_dates)
+elif inputFileExt in ['.xls', '.xlsx']:
+    df = pd.read_excel(inputFile, index_col=index_col, parse_dates=parse_dates)
+else:
+    print('not support input file format')
+    quit()
+
+bcr.bar_chart_race(df=df,filename=outputFile)
