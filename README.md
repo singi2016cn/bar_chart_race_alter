c30f7c7276c25986d23480354b7c0e4131ec9255
yi.qin <yi.qin@xshoppy.com>
2020-12-29 11:09:32 +0800

    init

--- README.md
+++ README.md
@@ -1,2 +1,25 @@
-# bar_chart_race_alter
-bar_chart_race alter
+# 动态条形图 bar_chart_race
+
+## 参考 reference
+
+- [bar_chart_race](https://github.com/dexplo/bar_chart_race)
+- [pandas](https://pandas.pydata.org/pandas-docs/stable/reference/api/pandas.read_csv.html)
+
+## 安装
+
+> 下载 [conda](https://docs.conda.io/en/latest/miniconda.html)
+
+```py
+pip install bar_chart_race
+conda install -c conda-forge bar_chart_race
+```
+
+
+## 使用 use
+
+> 运行项目根目录下的 `main.py`,视频默认生成在 `output/` 下，可自行修改 `main.py` 中生成参数，具体参考 `https://www.dexplo.org/bar_chart_race/tutorial/`
+
+```py
+python main.py
+```
+
---
+++ main.py
@@ -0,0 +1,34 @@
+import bar_chart_race as bcr
+import pandas as pd
+
+df = pd.read_csv("input/a.csv", index_col='date', parse_dates=['date'])
+bcr.bar_chart_race(
+    df=df,
+    filename='output/a.mp4',
+    orientation='h',
+    sort='desc',
+    n_bars=6,
+    fixed_order=False,
+    fixed_max=True,
+    steps_per_period=10,
+    interpolate_period=False,
+    label_bars=True,
+    bar_size=.95,
+    period_label={'x': .99, 'y': .25, 'ha': 'right', 'va': 'center'},
+    period_fmt='%B %d, %Y',
+    period_summary_func=lambda v, r: {'x': .99, 'y': .18,'s': f'Total deaths: {v.nlargest(6).sum():,.0f}','ha': 'right', 'size': 8, 'family': 'Courier New'},
+    perpendicular_bar_func='median',
+    period_length=500,
+    figsize=(5, 3),
+    dpi=144,
+    cmap='dark12',
+    title='COVID-19 Deaths by Country',
+    title_size='',
+    bar_label_size=7,
+    tick_label_size=7,
+    shared_fontdict={'family' : 'Helvetica', 'color' : '.1'},
+    scale='linear',
+    writer=None,
+    fig=None,
+    bar_kwargs={'alpha': .7},
+    filter_column_colors=False)  
