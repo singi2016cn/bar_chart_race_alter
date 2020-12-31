f559701ee3db901a973c77597b50354fbca633fd
yi.qin <yi.qin@xshoppy.com>
2020-12-31 14:08:36 +0800

    支持中文字体

--- main.py
+++ main.py
@@ -7,6 +7,7 @@ import time
 inputFile = 'input/a.csv' #输入数据路径
 index_col = 'date' #显示的列
 outputFile = 'output/' + str(time.time()) +'.mp4' #输出路径
+barNumber = 10 # 显示的条形图数量
 
 inputFileArr = os.path.splitext(os.path.basename(inputFile))
 inputFileName = inputFileArr[0]
@@ -23,4 +24,4 @@ else:
     print('not support input file format')
     quit()
 
-bcr.bar_chart_race(df=df,filename=outputFile)
+bcr.bar_chart_race(df=df,filename=outputFile, title='三国演义之谁才是主角', n_bars=barNumber, shared_fontdict={'family': 'Microsoft Yahei'})
